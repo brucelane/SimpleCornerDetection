0c7449280cd1b3e57aa89092bc0205aa9edf2f62
Bruce LANE <bruce@batchass.fr>
2013-09-26 09:13:23 +0200

    1st

---
+++ src/SimpleCornerDetectionApp.cpp
@@ -0,0 +1,88 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
+
+#include "cinder/Rand.h"
+#include "cinder/Capture.h"
+
+#include "CinderOpenCv.h"
+
+#include "markerdetector.h"
+#include "cvdrawingutils.h"
+
+using namespace ci;
+using namespace ci::app;
+
+class SimpleCornerDetectionApp : public AppNative {
+  public:
+   void prepareSettings( Settings *settings );
+	void setup();
+	void update();
+	void draw();
+    
+    gl::TextureRef          mInput;
+    Surface             	mSurf;
+    CaptureRef              mCapture;
+    
+    aruco::CameraParameters mCamParam;
+    aruco::MarkerDetector mMarkerDetector;
+    std::vector<aruco::Marker> mMarkers;
+	//gl::Texture	mTexture;
+};
+void SimpleCornerDetectionApp::prepareSettings( cinder::app::AppBasic::Settings *settings ) {
+    settings->setFrameRate( 60.0 );
+    settings->setWindowSize( 640, 480 );
+}
+void SimpleCornerDetectionApp::setup()
+{
+	// The included image is copyright Trey Ratcliff
+	// http://www.flickr.com/photos/stuckincustoms/4045813826/
+	
+	/*ci::Surface8u surface( loadImage( loadAsset( "dfw.jpg" ) ) );
+	cv::Mat input( toOcv( surface ) );
+	cv::Mat output;*/
+
+//	cv::medianBlur( input, output, 11 );
+//	cv::Sobel( input, output, CV_8U, 0, 1 ); 
+//	cv::threshold( input, output, 128, 255, CV_8U );
+
+	//mTexture = gl::Texture( fromOcv( output ) );
+	// CAMERA
+    try {
+		mCapture = Capture::create( 640, 480 );
+		mCapture->start();
+	}catch( ... ) {
+		console() << "Failed to initialize capture" << std::endl;
+	}
+}   
+void SimpleCornerDetectionApp::update(){
+    if( mCapture->checkNewFrame() ){
+        mSurf = mCapture->getSurface();
+        mInput = gl::Texture::create( mSurf );
+    }
+    if( mSurf ){
+        cv::Mat input = toOcv( mSurf );
+        mMarkerDetector.detect( input, mMarkers );
+    }
+}
+void SimpleCornerDetectionApp::draw()
+{
+	/*gl::clear();
+	gl::draw( mTexture );*/
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) );
+    gl::enableAlphaBlending();
+    gl::color( ColorA(1,1,1,1.0f) );
+    if( mInput ){
+        gl::draw( mInput );
+    }
+    gl::color( ColorA(1,1,1,0.7f) );
+    for( int i=0; i<mMarkers.size(); i++){
+        for( int ii = 0; ii < mMarkers[i].size(); ii++){
+            gl::drawSolidCircle(Vec2f(mMarkers[i][ii].x, mMarkers[i][ii].y), 10.0f);
+        }
+    }
+}
+
+CINDER_APP_NATIVE( SimpleCornerDetectionApp, RendererGl )
